ed72b5543fe897b95075f2dc95451152b87240d9
Parker Boeing <parker.boeing@gmail.com>
2023-04-12 15:39:02 -0600

    Add different conditional statements for plants

--- plants.rb
+++ plants.rb
@@ -1,7 +1,11 @@
-gets_sunshine = true
+gets_sunshine = false
 ounces_of_water = 1
 if ounces_of_water >= 10 && gets_sunshine == true
     puts "Plants alive and thriving!"
+elsif ounces_of_water < 10 && gets_sunshine == true
+    puts "Plants are thirsty, get more water!"
+elsif ounces_of_water >= 10 && gets_sunshine == false
+    puts "Your plants need to see the light!"
 else
-    puts "Your plant died!"
+    puts "You have the black thumb of death!"
 end
